a9ab93f135d6e7f0104abccb5e28641034aea744
Nik-9649 <usmanfazaljj@gmail.com>
2022-08-06 00:46:37 +0500

    Maps

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
 // Data needed for a later exercise
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
 // Data needed for first part of the section
-const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'] // a part of enhanced object literals
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]; // a part of enhanced object literals
 
 const openingHours = {
   [weekdays[3]]: {
@@ -23,29 +23,38 @@ const openingHours = {
 };
 
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
   openingHours,
 
   order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery({ starterIndex = Math.trunc(Math.random() * 3 + 1), mainIndex = Math.trunc(Math.random() * 2 + 1), address, time = `06:00` }) {
-    console.log(`Order recived! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  orderDelivery({
+    starterIndex = Math.trunc(Math.random() * 3 + 1),
+    mainIndex = Math.trunc(Math.random() * 2 + 1),
+    address,
+    time = `06:00`,
+  }) {
+    console.log(
+      `Order recived! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
   },
 
   orderPasta(ing1, ing2, ing3) {
-    console.log(`Here is your delicous pasta with ${ing1}, ${ing2}, and ${ing3}!!`);
+    console.log(
+      `Here is your delicous pasta with ${ing1}, ${ing2}, and ${ing3}!!`
+    );
   },
 
   orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
-  }
+  },
 };
 
 /*
@@ -416,21 +425,34 @@ console.log(staff.size);
 
 // 14 Maps
 const rest = new Map();
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze, Italy');
-rest.set(2, 'Lisbon, Portugal');
+rest.set("name", "Classico Italiano");
+rest.set(1, "Firenze, Italy");
+rest.set(2, "Lisbon, Portugal");
 
-console.log(
-  rest
-    .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
-    .set("open", 11)
-    .set("close", 23)
-    .set(true, "we are open :D")
-    .set(false, "we are closed :(")
-);
+rest
+  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+  .set("open", 11)
+  .set("close", 23)
+  .set(true, "we are open :D")
+  .set(false, "we are closed :(");
 
-console.log(rest.get('name'));
-console.log(rest.get(true));
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
 
 const time = 8;
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+
+// console.log(rest.has(`categories`));
+
+rest.delete(2);
+
+const arr = [1, 2];
+rest.set(arr, "test");
+console.log(rest);
+
+console.log(rest.size);
+console.log(rest.get(arr));
+// rest.clear();
+// console.log(rest);
+
+console.log(rest.set(document.querySelector("h1"), "Heading"));
